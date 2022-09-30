652c5429b2cc6a13fa8bf2ec0f0d676969267573
Anaada <anaada2015@gmail.com>
2022-09-30 11:24:43 +0600

    privacy

---
+++ src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,246 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import MainLayout from "../layouts/mainLayout/MainLayout";
+
+function PrivacyPolicy() {
+  return (
+    <MainLayout>
+      <div style={{ marginTop: "10px" }}></div>
+      <Card>
+        <Card.Body>
+          <h3>Privacy Policy</h3>
+          <p>Last updated: Sept 29, 2022</p>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <h5>Welcome to AonMart!</h5>
+          <p>
+            AonMart – Your trusted source for all your Organic Food and other
+            family needs. Buy online or visit our store. AonMart takes your
+            privacy seriously, and we are committed to maintaining the privacy
+            of the personal information that you provide to us. By using AonMart
+            you acknowledge that the terms and conditions in this Privacy Policy
+            will apply to you, as well as to any information that you give us.
+            Please take the time to read through this Privacy Policy to get
+            acquainted with our practices. We sometimes use capitalized terms in
+            this Privacy Policy; we will use AonMart throughout this Privacy
+            Policy when referring to the AonMart service or platform and Mobile
+            Applications (the “Service” or the “Platform”) that we provide on
+            the web at www.AonMart.com and on various mobile operating systems.
+          </p>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <h5>1. Information We Collect, and How We Use It.</h5>
+          <p style={{ marginTop: "10px" }}>
+            We collect different types of information through the Platform,
+            including: information that you provide us directly; information
+            about you that we automatically collect from log data, IP addresses,
+            and aggregated user data; cookies; information that you give us
+            about your friends; and your chat histories. We may use this
+            information in different ways. A. Information that you provide us
+            directly What is it? Information you give us when you register an
+            account with the Platform (this might include your first and last
+            name, username, phone number, email address, password, date of
+            birth/age, and other personal information); Information that you
+            post on your AonMart user profile; All other content that you
+            post/share on any part of the Platform (this might include photos,
+            videos, and any other content that you post, or associate with your
+            account); Information you give us during any communications you have
+            with us (this might include information you give us in emails you
+            send us, chats you have with us, anything you send us in the mail,
+            and any other information you might send us); Interactions that you
+            have with other users or content on the Platform; and When you give
+            us authorization, contact information of your friends from your
+            mobile or tablet devices to connect your contacts and address book
+            information with your AonMart account. How do we use it? To enforce
+            our Terms of Service; To respond to your comments and questions; To
+            customize your experience, to test and introduce new features and
+            products, and to improve and monitor the effectiveness of our
+            Platform; To carry out contests, surveys, evaluations, and other
+            events or activities that you choose to participate in; For any
+            other purposes that we specifically communicate to you when we
+            collect that information; To communicate with you regarding your use
+            of the Platform, and to advise you of changes to the Platform; To
+            find and connect you with your friends and contacts who are AonMart
+            users and to find and invite your contacts to connect with you on
+            AonMart; and To use your email address or other contact information
+            to send you articles, news, updates, newsletters, marketing
+            materials, or any other information related to the Platform/
+            Platform services. Other things you should know: We may remove, at
+            our sole discretion, any information that you provide when you
+            register for or use the Platform (such as usernames, profile
+            pictures, names, etc.), if we believe it is inappropriate or
+            violates any of the policies posted on our Policies page at
+            www.AonMart.com/policies, including, but not limited to, our Terms.
+            B. Information that we automatically collect: What is it? We
+            automatically collect information about your computer, mobile
+            device, tablet, and other devices when you use our Platform. Among
+            other things, we collect session data, IP addresses, web browser
+            software information, and mobile device identifiers such as your
+            device’s UDID. We may also collect and/or aggregate information
+            about your online activities, including but not limited to, content
+            you view, demographic patterns, elements of the Service that you
+            use, and pages you visit. How do we use it? We may analyze,
+            evaluate, and/or aggregate this information to help us understand
+            your interests and the interests of our other users, to analyze
+            trends, to optimize your user experience and improve the Platform.
+            Other things you should know: We may share aggregated information
+            about the Platform/Service and our users, without seeking additional
+            consent, with third parties (including, without limitation,
+            advertisers, media, and actual or potential partners or investors)
+            for informational, marketing, and/or promotional purposes. If we do
+            share that information, we will only share it on an aggregated basis
+            and in a manner that does not reveal any personally identifiable
+            information about any individual user. C. Information you give us
+            about your friends What is it? If you choose to use the Platform to
+            tell a friend about us, we will ask you for and store your friend’s
+            email address and/or phone number. We also might ask you to access
+            your phonebook, Facebook, Twitter, Google+, or other social media
+            sites contacts for this purpose. How do we use it? If you choose to
+            use the Platform/Service to invite one of your contacts to use the
+            Platform/Service, we will send your friend an email or SMS inviting
+            them to visit the Platform/Service. We use this information solely
+            to send these emails or SMS messages, to help your friends register
+            with the Platform/Service, and to track the success of our
+            invitation program. Your friend can contact us at
+            support@AonMart.com anytime to ask us to remove this information
+            from our database. D. Location Information What is it? When you use
+            our Mobile App, we may collect and store information about your
+            location by converting your IP address into a rough geo-location or
+            by accessing your mobile device’s GPS coordinates or coarse location
+            if you enable location services on your device. How do we use it? To
+            improve and personalize our Services, including, without limitation,
+            to identify nearby Users with whom users can communicate. If you are
+            a User or if you communicate with a User, we may, review, scan, or
+            analyze any communications via the Platform/Services involving a
+            User for fraud prevention, risk assessment, and customer support
+            purposes. We will not review, scan, or analyze your communications
+            for sending third party marketing messages to you. We will also not
+            sell these reviews or analyses of communications to third parties.
+            We will also use automated methods to carry out these reviews or
+            analyses where reasonably possible. However, from time to time we
+            may have to manually review some communications. To collect
+            aggregated data from your usage history. We use the data to build
+            language models which help us improve the Platform/Services. We will
+            only collect this data on an aggregated basis, and in a manner which
+            does not reveal any personally identifiable information about any
+            individual user. We will use this data to help us understand our
+            users’ interests, analyze trends, and customize the user experience.
+          </p>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <h5>Sharing Your Information.</h5>
+          <p style={{ marginTop: "10px" }}>
+            In addition to using your information for the purposes described
+            above, we may share your information with parties other than AonMart
+            or its users (the “Third Parties”). In some cases we may request
+            your permission before we share that information, but we reserve the
+            right to share your information with Third Parties without prior
+            notification or without soliciting additional permission: We discuss
+            below situations where we may share your information without
+            requesting your further permission: We share certain information
+            about you (including information which could be used to personally
+            identify you) with the companies we engage to facilitate our
+            Platform/Services, perform Platform/Services -related functions,
+            and/or provide the Platform/Services on our behalf, such as Internet
+            Service Providers and hosting service providers. We require these
+            companies to maintain your information in confidence, and they are
+            prohibited from using your information in any way other than
+            performing services for or on behalf of AonMart. We may disclose
+            your information (including information which could be used to
+            personally identify you) when we have a good faith belief that
+            disclosure is necessary: (i) to respond to subpoenas, court orders,
+            or other compulsory legal process; (ii) to protect our property
+            rights and investigate and defend against legal claims (iii) to
+            enforce or apply our Privacy Policy, our Terms or our other policies
+            or agreements; (iv) when we believe disclosure is necessary to
+            protect others from death or serious bodily injury; and (v) as
+            otherwise required by law. Transfer your information in the event
+            that AonMart is involved in a merger, acquisition, divestiture,
+            consolidation, reorganization, asset sale, or bankruptcy.
+          </p>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <h5>Controlling Your Information.</h5>
+          <p style={{ marginTop: "10px" }}>
+            A. Posting in Chat Groups When you post or make content available in
+            Chat Groups, that content will be visible to everyone who is a
+            member of that Chat Group. B. Communicating with Users When you
+            communicate with or make content available to a User that content is
+            provided directly to the User and is subject to the User’s privacy
+            or security practices and policies. C. Accessing and Changing your
+            Account Information; Deleting your Account You can edit, update, or
+            delete your account at any time. If you delete your account, you
+            won’t be able to sign in, use the Platform/Services, or access any
+            of your information. If you delete your account, we may retain
+            certain information associated with your account, for analytical
+            purposes and recordkeeping. If you delete your account with us,
+            please be aware that we may retain any content you have previously
+            posted on the Platform/Services, including, without limitation, any
+            other data or posted content. D. Opting out of Receiving
+            Communications If you are using the Platform/Services, you are to be
+            open to receiving communications from us. You won’t be able to opt
+            out of Platform/Services -related communications like
+            Platform/Services announcements, administrative notices, and the
+            like.
+          </p>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <h5>Security.</h5>
+          <p style={{ marginTop: "10px" }}>
+            Your account, registration information, and password are protected
+            so that only you (or any third parties that you allow to access your
+            login credentials) can access your account. All personally
+            identifiable information that we collect about you is stored in
+            limited-access servers. The Platform/Services has technical,
+            administrative, and physical security measures in place to protect
+            against the loss, misuse and alteration of the information which is
+            under our control. While we are committed to implementing reasonable
+            and appropriate safeguards to protect your information against
+            foreseeable risks, no computer system or information can ever be
+            fully protected against any possible risk, and we cannot guarantee
+            that unauthorized third parties will not be able to circumvent those
+            safeguards, or access and/or use your personal information for
+            improper purposes.
+          </p>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <h5>Changes to our Privacy Policy.</h5>
+          <p style={{ marginTop: "10px" }}>
+            We may modify our Privacy Policy from time to time. When we make
+            changes, we will notify you by making the revised version available
+            on this webpage, and will indicate at the top the date of the latest
+            revision. You should check this Privacy Policy on a regular basis,
+            since revised versions will be binding on you. Any such
+            modifications will be effective when we post the updated Privacy
+            Policy. You understand and agree that your continued access to our
+            Platform/Services after any posted modification to the Privacy
+            Policy indicates your acceptance of those modifications.
+          </p>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <h5>Contact Us</h5>
+          <p style={{ marginTop: "10px" }}>
+            If you have any questions about this Privacy Policy, please contact
+            us online at <h5>support@AonMart.com.</h5>
+          </p>
+        </Card.Body>
+      </Card>
+    </MainLayout>
+  );
+}
+
+export default PrivacyPolicy;
